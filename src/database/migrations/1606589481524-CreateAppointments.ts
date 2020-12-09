e6d6c3f34c431adfbc63a48b6ffa53c3072dd451
Kleiton Marques <kleitonm.marques@gmail.com>
2020-12-09 21:22:15 -0200

    Create relationship in models

--- src/database/migrations/1606589481524-CreateAppointments.ts
+++ src/database/migrations/1606589481524-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1606589481524 implements MigrationInterfa
 				columns: [
 					{
 						name: 'id',
-						type: 'varchar',
+						type: 'uuid',
 						isPrimary: true,
 						generationStrategy: 'uuid',
 						default: 'uuid_generate_v4()',
