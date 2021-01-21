3a5dc83b7e977cd7b750e88744b901201d1e1126
Kleiton Marques <kleitonm.marques@gmail.com>
2021-01-21 18:10:50 -0200

    Password encription

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,13 +8,22 @@ UsersRouter.post('/', async (request, response) => {
   try {
 	const { name, email, password } = request.body
 	const createUser = new CreateUserService()
+
 	const user = await createUser.execute({
 		name,
 		email,
 		password
 	})
 
-	return response.json(user)
+    const userWithoutPassword = {
+		id: user.id,
+		name: user.name,
+		email: user.email,
+		created_at: user.created_at,
+		updated_at: user.updated_at,
+	}
+
+	return response.json(userWithoutPassword)
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
