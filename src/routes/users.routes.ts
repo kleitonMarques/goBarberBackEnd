eeedaf1010516291a7797641a8a378c0e55a66df
Kleiton Marques <kleitonm.marques@gmail.com>
2021-01-29 18:29:41 -0200

    Upload file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import 'reflect-metadata';
 import { Router } from 'express';
+import multer from 'multer'
+import uploadConfig from '../config/upload'
+
 import CreateUserService from '../services/CreateUsersService'
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -28,5 +34,10 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+	console.log(request.file)
+	return response.json({ ok: true })
+})
 
 export default usersRouter;
